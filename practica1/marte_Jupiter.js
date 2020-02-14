a0b649305bb1f2a19aabe96784321e2ef1ebab9f
Carlos Navia <cenaviat@gmail.com>
2020-02-14 08:06:58 -0500

    manejo de condiciones con programa de peso en planetas - reto implementado

--- practica1/marte_Jupiter.js
+++ practica1/marte_Jupiter.js
@@ -5,15 +5,19 @@ var peso = parseInt (usuario)
 var g_tierra = 9.8
 var g_marte = 3.7
 var g_jupiter = 24.8
+var nombrePlaneta
 var peso_final
 
 if(planeta == 1){
     peso_final = peso * g_marte / g_tierra
+    nombrePlaneta = "Marte"
 }else if (planeta == 2) {
     peso_final = peso * g_jupiter / g_tierra
+    nombrePlaneta = "Jupiter"
 }else{
     peso_final = 10000000
+    nombrePlaneta = " "
 }
 
 peso_final = parseInt(peso_final)
-document.write("Tu peso en Marte es de: <strong>" + peso_final + " Kilos <strong/>")
+document.write("Tu peso en "+nombrePlaneta+" es de: <strong>" + peso_final + " Kilos <strong/>")
