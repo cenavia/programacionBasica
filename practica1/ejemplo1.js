e7d825db32d5c2c700d663c06407424fe3c9b4ab
Carlos Navia <cenaviat@gmail.com>
2020-02-14 07:41:43 -0500

    mejora algoritmo peso en marte usando prompt y parseando los valores a enteros

--- practica1/ejemplo1.js
+++ practica1/ejemplo1.js
@@ -1,9 +1,11 @@
+var usuario = prompt("cual es tu Peso")
+var peso = parseInt (usuario)
+
 var g_tierra = 9.8
 var g_marte = 3.7
 var g_jupiter = 24.8
-var peso = 77
 var peso_final
 
 peso_final = peso * g_marte / g_tierra
 peso_final = parseInt(peso_final)
-document.write("Tu peso en Marte es de: ", peso_final)
+document.write("Tu peso en Marte es de: <strong>" + peso_final + " Kilos <strong/>")
